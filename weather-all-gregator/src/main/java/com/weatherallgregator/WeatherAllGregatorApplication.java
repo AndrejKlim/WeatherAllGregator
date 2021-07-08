857b21a6c50f775738ac2606dfac93840ba6dc18
andreiklim <andreiklim@instinctools.ru>
2021-07-08 12:47:15 +0300

    Initial commit

---
+++ weather-all-gregator/src/main/java/com/weatherallgregator/WeatherAllGregatorApplication.java
@@ -0,0 +1,13 @@
+package com.weatherallgregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAllGregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAllGregatorApplication.class, args);
+    }
+
+}
